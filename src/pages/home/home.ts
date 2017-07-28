13e5f1d163b830030cc723fabe5770906abd9900
Ashik Alam <aalam@webmd.net>
2017-07-28 16:26:38 -0400

    Im not getting the final ngFor call..

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -62,83 +62,88 @@ export class HomePage {
     //we'll save this in the future
     this.projectFiles = this.github.getDataCached(this.projectTreeURL).map(data => {
 
-        // /[a-zA-Z\_][a-zA-Z0-9\_]*/g
+      // /[a-zA-Z\_][a-zA-Z0-9\_]*/g
 
-        //path
-        //type
-        let projectFileObjects = [];
+      //path
+      //type
+      let projectFileObjects = [];
+      // let fileExtensionRegex = /(?:\.([^.]+))?$/;
 
+      //
+      // data.tree[2].map(file => { //why doens this work?
+      data.tree.forEach(file => {
+
+          let rawCodeString;
+          // console.log(file);
+
+        // if (file.type === "blob") {
+        //   var fileExtension = file.path.split('.').pop();
+        //   console.log(fileExtension);
         //
-        // data.tree[2].map(file => { //why doens this work?
-        data.tree.forEach(file => {
-
-            let rawCodeString;
-            // console.log(file);
+        // }
 
 
-            //TODO: what about no nblobs??
-            // if (file.type === "blob") {
-            if (file.type === "blob" && file.path.includes('xhr-node.js')) { //testing one file
-              this.github.getRawCodeCached(file.url).subscribe(codeString => {
-                rawCodeString = codeString;
-                // console.log('getRawCodeCached RESPONSE');
-                // console.log(codeString);
+          //TODO: what about no nblobs??
+          // if (file.type === "blob") {
+          if (file.type === "blob") { //testing one file
+            this.github.getRawCodeCached(file.url).subscribe(codeString => {
+              rawCodeString = codeString;
+              // console.log('getRawCodeCached RESPONSE');
+              // console.log(codeString);
 
-                // console.log('rawCodeString');
-                // console.log('rawCodeString');
-                // console.log(rawCodeString);
+              // console.log('rawCodeString');
+              // console.log('rawCodeString');
+              // console.log(rawCodeString);
 
-                //TODO: reference this code block
-                //TODO: parse value...
-                //TODO: file type check..
-                // let test;
-                // test = Highlightjs.highlight('javascript', rawCodeString);
-                //
-                // console.log('test');
-                // console.log(test);
-                // file.code = test.value;
-                file.language = 'javascript';
-                file.code = rawCodeString;
-                projectFileObjects.push(file);
-                // data.code =
-                // console.log('FILE');
-                // console.log(file.path);
-                // console.log(codeString.substring(0, 10));
-                // projectFileObjects.push(file)
-              });
-            }
-
-
-            // console.log(file.path);
-            // console.log(rawCodeString);
-
-            // projectFileObjects.push(file);
-            // console.log(file);
+              //TODO: reference this code block
+              //TODO: parse value...
+              //TODO: file type check..
+              // let test;
+              // test = Highlightjs.highlight('javascript', rawCodeString);
+              //
+              // console.log('test');
+              // console.log(test);
+              // file.code = test.value;
+              var fileExtension = file.path.split('.').pop();
+              file.language = fileExtension;
+              file.code = rawCodeString;
+              projectFileObjects.push(file);
+              // data.code =
+              // console.log('FILE');
+              // console.log(file.path);
+              // console.log(codeString.substring(0, 10));
+              // projectFileObjects.push(file)
+            });
           }
-        );
 
 
-        // console.log('data');
-        // console.log(data);
-        console.log('done');
+          // console.log(file.path);
+          // console.log(rawCodeString);
 
-        console.log('post-processing1');
-        console.log(projectFileObjects);
+          // projectFileObjects.push(file);
+          // console.log(file);
+        }
+      );
 
 
-        // let codeBlocks1 = document.getElementsByClassName("code-block");
-        // console.log(codeBlocks1.length);
+      // console.log('data');
+      // console.log(data);
+      console.log('done');
 
-        return projectFileObjects;
-        //TODO: apply post semantic hightlighting
-        //TODO: for each pre code bloock..
+      console.log('post-processing1');
+      // console.log(projectFileObjects);
 
-        //need to return finalized array
-        // return projectFileObjects;
-      },
-      {
-        data: console.log('yellah'),
-      }); //woot this works
+
+      // let codeBlocks1 = document.getElementsByClassName("code-block");
+      // console.log(codeBlocks1.length);
+
+      return projectFileObjects;
+      //TODO: apply post semantic hightlighting
+      //TODO: for each pre code bloock..
+
+      //need to return finalized array
+      // return projectFileObjects;
+    });
 
 
     console.log('post-processing2');
@@ -185,14 +190,20 @@ export class HomePage {
 
   ngAfterViewInit() {
     this.things.changes.subscribe(t => {
-      this.ngForRendred();
+      this.ngForRendered();
     })
   }
 
-  ngForRendred() {
+  ngForRendered() {
     console.log('NgFor is Rendered');
     Highlightjs.initHighlighting();
 
+
+    //TODO: feature semantic highlighting...
+    // this.applySemanticHighlighting();
+
+
+    // console.log(g.eel[0].childNodes);
   }
 
   changeFontSize() {
@@ -201,8 +212,89 @@ export class HomePage {
     code.style.fontSize = this.fontSize + 'px';
   }
 
-  addhljs() {
-    Highlightjs.initHighlighting();
+
+  applySemanticHighlighting() {
+    let codeBlocks = document.getElementsByClassName("code-block");
+    console.log(codeBlocks.length);
+    let g: any = window;
+    g.eel = codeBlocks;
+    g.tt = [];
+    g.tk = {};
+
+    // need to take out spans and see if wrapping text here works..
+    let parentNode = g.eel[0];
+
+
+    let keywordRegex = /[a-zA-Z\_][a-zA-Z0-9\_]*/g;
+
+    g.eel[0].childNodes.forEach(function (e) {
+
+      // if (e === 30){
+      //   break;
+      // }
+
+      if (e.nodeName === "#text") {
+        //do semantic stuff here
+        g.tt.push(e);
+
+        //TODO: extract semantic text
+        //TODO: unique text cache with color
+        //TODO span only part of text
+
+        //TODO: DIL param scopes..
+
+        //our cache build up..
+        let str = e.data;
+        let res = str.match(keywordRegex);
+        console.log('KEYWROD EXTRACTIOn');
+        if (res){
+          res.forEach(key =>{
+            g.tk[key] = key;
+          })
+        }
+        console.log(res);
+
+
+        var spanSemantic: any;
+        spanSemantic = document.createElement("span");
+        spanSemantic.style = 'color: red;';
+        var spanSemanticText = document.createTextNode(e.data);
+        spanSemantic.appendChild(spanSemanticText);
+        //replaceChild(new, old)
+        parentNode.replaceChild(spanSemantic, e);
+
+        return e;
+      } else {
+        if (e.childNodes.length > 3) {
+          // console.log('e with great length1!!!');
+          // console.log(e);
+          let parentNode2 = e;
+          e.childNodes.forEach(function (e2) {
+            // console.log('CHILD');
+            // console.log(e2);
+            // console.log(e2);
+            if (e2.className === "hljs-params") {
+              console.log('found PARAMs');
+              // console.log(e2.data);
+              console.log(e2);
+              console.log(e2);
+              g.tt.push(e2);
+
+              var spanSemantic: any;
+              spanSemantic = document.createElement("span");
+              spanSemantic.style = 'color: blue;';
+              var spanSemanticText = document.createTextNode(e2.textContent);
+              spanSemantic.appendChild(spanSemanticText);
+              parentNode2.replaceChild(spanSemantic, e2);
+
+              return e2;
+            }
+          });
+        }
+      }
+
+
+    });
   }
 
   // application/vnd.github.v3.raw
